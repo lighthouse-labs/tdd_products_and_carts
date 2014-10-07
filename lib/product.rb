5a469c04c0339851833c0fc83ec68fdcbd8340e0
Monica Olinescu <monica.olinescu@gmail.com>
2014-10-07 10:51:23 -0700

    Test the product class without rspec
    Wrote some ruby code to use our simple product class in
    `spec/product_spec.rb`. This ruby code lets us check if the Product
    class works.

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,10 @@
 class Product
+  attr_reader :name, :price_in_cents
+
+  DEFAULT_PRICE = 0
+
+  def initialize(name, price_in_cents = DEFAULT_PRICE)
+    @name = name
+    @price_in_cents = price_in_cents
+  end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,5 +1,34 @@
 require_relative "spec_helper"
 
-describe "Product" do
+# Test that I can use the initializer
+muffin = Product.new("muffin", 199)
+# Make sure these values are stored inside the object
 
+if muffin.name != "muffin"
+  @fail = true
+  puts "FAIL! Name should be 'muffin' but got #{muffin.name} instead"
+end
+
+if muffin.price_in_cents != 199
+  @fail = true
+  puts "FAIL! Name should be '199' but got #{muffin.price_in_cents} instead"
+end
+
+if !@fail
+  puts "PASSED"
+end
+
+
+# Test that price defaults to 0
+@fail = false
+coffee = Product.new("coffee")
+
+if coffee.price_in_cents != 0
+  @fail = true
+  puts "FAIL! Price should default to 0, but I got #{coffee.price_in_cents}"
+end
+
+
+if !@fail
+  puts "PASSED"
 end

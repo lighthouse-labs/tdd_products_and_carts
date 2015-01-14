76a2feea41647d10d05f211d9989abbe53f2021c
Don Burks <don@lighthouselabs.ca>
2015-01-14 10:36:56 -0800

    Code for January cohort demonstration

---
+++ lib/invalid_product_error.rb
@@ -0,0 +1,3 @@
+class InvalidProductError < StandardError
+
+end
--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,7 @@
 class Product
-  attr_reader :name, :price_in_cents
-
-  DEFAULT_PRICE = 0
-
-  def initialize(name, price_in_cents = DEFAULT_PRICE)
-    @name = name
-    @price_in_cents = price_in_cents
-  end
+	attr_accessor :name, :price_in_cents
+	def initialize(name, price_in_cents=0)
+		@name = name
+		@price_in_cents = price_in_cents
+	end
 end

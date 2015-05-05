55b0726723626a8c8a4c5ae55198716210729b07
Monica Olinescu <monica.olinescu@gmail.com>
2015-05-05 16:19:51 -0700

    Refactor cart_spec to use before :each for all tests

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -2,15 +2,22 @@ require_relative "spec_helper"
 
 describe "Cart" do
 
+  before :each do
+    # ruby code here runs before every test case inside this describe block
+    @cart = Cart.new
+  end
+  # another way of accomplishing the above
+  # let(:cart) { Cart.new }
+  
   describe "#initialize" do
     it "should have empty list of products" do
-      cart = Cart.new
+      @cart = Cart.new
       # expect(cart.products.length).to eql 0
       # expect(cart.products).to be_empty
       # cart.products.empty?
       # expect(cart.products).to be_nil
       # cart.products.nil?
-      expect(cart.products).to eql []
+      expect(@cart.products).to eql []
     end
   end
 
@@ -18,21 +25,15 @@ describe "Cart" do
 
     it "should add a product to the list of products" do
       product = Product.new(name: "Book", price_in_cents: 1299)
-      cart = Cart.new
-      cart.add_product(product)
+      @cart = Cart.new
+      @cart.add_product(product)
       # expect(cart.products.include?(product)).to eql true
-      expect(cart.products).to include product
+      expect(@cart.products).to include product
     end
   end
 
   describe "#total_with_tax" do
 
-    before :each do
-      # ruby code here runs before every test case
-      @cart = Cart.new
-    end
-    # another way of accomplishing the above
-    # let(:cart) { Cart.new }
 
     it "should return 0 if cart is empty" do
       expect(@cart.total_with_tax).to eql 0

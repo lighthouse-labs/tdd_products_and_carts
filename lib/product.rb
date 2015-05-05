20e845f7037a2b167a2403b3fe7f22afdd42b62b
Monica Olinescu <monica.olinescu@gmail.com>
2015-05-05 11:11:55 -0700

    Ability to create a product

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,7 @@
 class Product
+  attr_reader :name, :price_in_cents
+  def initialize(name: name, price_in_cents: price_in_cents)
+    @name = name
+    @price_in_cents = price_in_cents
+  end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,4 +2,13 @@ require_relative "spec_helper"
 
 describe "Product" do
 
+  describe '#new' do
+
+    it 'works' do # a test case
+      product = Product.new(name: 'Book', price_in_cents: 1299)
+      expect(product.name).to eql 'Book'
+      expect(product.price_in_cents).to eql 1299
+    end
+  end
+
 end

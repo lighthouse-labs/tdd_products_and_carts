21f5b330b35d923126abeecd5f7ce50fbf2b1ced
Monica Olinescu <monica.olinescu@gmail.com>
2015-05-05 11:12:14 -0700

    Throw an exception if Product name or price are missing

--- lib/product.rb
+++ lib/product.rb
@@ -1,6 +1,13 @@
+class InvalidProductError < StandardError
+end
+
 class Product
   attr_reader :name, :price_in_cents
+
   def initialize(name: name, price_in_cents: price_in_cents)
+    if name.nil? || price_in_cents.nil?
+      raise InvalidProductError, "Invalid product. Must have name and price in cents"
+    end
     @name = name
     @price_in_cents = price_in_cents
   end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -9,6 +9,12 @@ describe "Product" do
       expect(product.name).to eql 'Book'
       expect(product.price_in_cents).to eql 1299
     end
+
+    it 'throws exception if name or price are missing' do
+      expect {
+        product = Product.new
+      }.to raise_error InvalidProductError, "Invalid product. Must have name and price in cents"
+    end
   end
 
 end

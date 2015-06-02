e5f964384a36ffa8afec0a315ce449cc82b79ff8
Monica Olinescu <monica.olinescu@gmail.com>
2015-06-02 15:16:47 -0700

    Refactor cart tests to use describe

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,57 +1,70 @@
 require_relative "spec_helper"
 
 describe "Cart" do
-  it "should have no products when the cart is created" do
-    # prep
-    cart = Cart.new
 
-    # expectations
-    expect(cart.products).to be_empty
-    # expect calls: cart.products.empty?
-    # expect(cart.products).to be_nil
-    # expect calls: cart.products.nil?
+  describe "#initialize" do
+    it "should have no products when the cart is created" do
+      # prep
+      cart = Cart.new
+
+      # expectations
+      expect(cart.products).to be_empty
+      # expect calls: cart.products.empty?
+      # expect(cart.products).to be_nil
+      # expect calls: cart.products.nil?
+    end
   end
 
-  it "should add a product to the cart" do
-    # prep
-    cart = Cart.new
-    tooth_paste = Product.new("Tooth paste", 299)
-    cart.add_product(tooth_paste)
+  describe "#add_product" do
+    it "should add a product to the cart" do
+      # prep
+      cart = Cart.new
+      tooth_paste = Product.new("Tooth paste", 299)
+      cart.add_product(tooth_paste)
 
-    # expectations
-    expect(cart.products).to eql([tooth_paste])
-    # expect(cart.products).to match_array([tooth_paste])
+      # expectations
+      expect(cart.products).to eql([tooth_paste])
+      # expect(cart.products).to match_array([tooth_paste])
 
-    # Not so great, because if we add the product twice by mistake
-    # the test still passes
-    # expect(cart.products.include?(tooth_paste)).to be_true
-    # expect(cart.products).to be_include(tooth_paste)
-    # expect(cart.products).to include(tooth_paste)
+      # Not so great, because if we add the product twice by mistake
+      # the test still passes
+      # expect(cart.products.include?(tooth_paste)).to be_true
+      # expect(cart.products).to be_include(tooth_paste)
+      # expect(cart.products).to include(tooth_paste)
 
+    end
+
+    # brittle test
+    # it "should return the total with tax" do
+    #   cart = Cart.new
+    #   tooth_paste = Product.new("Tooth paste", 100)
+    #   brush = Product.new("Brush", 200)
+    #   cart.add_product(tooth_paste)
+    #   cart.add_product(brush)
+    #   
+    #   expect(cart.total_with_tax).to eql 336
+    # end
   end
 
-  # brittle test
-  # it "should return the total with tax" do
-  #   cart = Cart.new
-  #   tooth_paste = Product.new("Tooth paste", 100)
-  #   brush = Product.new("Brush", 200)
-  #   cart.add_product(tooth_paste)
-  #   cart.add_product(brush)
-  #   
-  #   expect(cart.total_with_tax).to eql 336
-  # end
+  describe "#total_with_tax" do
+    it "should return the total with tax" do
+      cart = Cart.new
+      tooth_paste = Product.new("Tooth paste", 100)
+      brush = Product.new("Brush", 200)
+      expect(cart).to receive(:products).and_return([tooth_paste, brush])
+      # Line above is the functional equivalent of
+      # cart.products = [tooth_paste, brush]
 
-  it "should return the total with tax" do
-    cart = Cart.new
-    tooth_paste = Product.new("Tooth paste", 100)
-    brush = Product.new("Brush", 200)
-    expect(cart).to receive(:products).and_return([tooth_paste, brush])
-    # Line above is the functional equivalent of
-    # cart.products = [tooth_paste, brush]
+      # I expect the lines below to call the method products on cart
+      # I am hacking the method products inside cart to return [tooth_paste, brush]
 
-    # I expect the lines below to call the method products on cart
-    # I am hacking the method products inside cart to return [tooth_paste, brush]
+      expect(cart.total_with_tax).to eql 336
+    end
 
-    expect(cart.total_with_tax).to eql 336
+    it "should return 0 if there are no products in the cart" do
+      cart = Cart.new
+
+      expect(cart.total_with_tax).to eql 0
+    end
   end
 end

2e2d46aec39b07864a40a84de0b9bed848257304
Monica Olinescu <monica.olinescu@gmail.com>
2015-06-30 10:28:38 -0700

    Basic product and cart code

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -2,16 +2,13 @@ require_relative "spec_helper"
 
 describe "Cart" do
   describe "#add_product" do
-    # Block runs before each 'it' block.
-    before :each do
-      @cart = Cart.new
-    end
+    let :cart, { Cart.new }  # runs block before every it test case. Assigns result of block to local variable
 
     it "should have an empty list of products for a new cart" do
       # Setup
       # cart = Cart.new
       # Expectation
-      expect(@cart.products).to eql []
+      expect(cart.products).to eql []
       # expect(cart.products).to match_array []
       # expect(cart.products).to have(0).things
     end
@@ -20,9 +17,9 @@ describe "Cart" do
       # Setup
       # cart = Cart.new
       product = Product.new("Book", 1299)
-      @cart.add_product(product)
+      cart.add_product(product)
       # Expectations
-      expect(@cart.products).to eql [product]
+      expect(cart.products).to eql [product]
     end
   end
 end
